51cc20b4e5378831a799038e11c302ba21060bf0
Rajesh <rbj@narola.email>
2020-02-12 18:06:52 +0530

    Add sales person form

---
+++ BG/Areas/Admin/Controllers/SalesPersonController.cs
@@ -0,0 +1,28 @@
+﻿using BG.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BG.Areas.Admin.Controllers
+{
+    public class SalesPersonController : BaseController
+    {
+        // GET: Admin/SalesPerson
+        public ActionResult Index()
+        {
+            if (!CurrentUser.Identity.IsAuthenticated)
+            {
+                return RedirectToAction("Login", "Account", new { area = "" });
+            }
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult New()
+        {
+            return View();
+        }
+    }
+}
