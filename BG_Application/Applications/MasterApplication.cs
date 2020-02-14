3cbada2c6306a4ac06b47cb1fdf481858919c679
Rajesh <rbj@narola.email>
2020-02-14 12:59:05 +0530

    Add swagger and application

---
+++ BG_Application/Applications/MasterApplication.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BG_DataModel.DataModel;
+
+namespace BG_Application.Applications
+{
+    public class MasterApplication
+    {
+        private readonly BG_DBEntities _db;
+    }
+}
