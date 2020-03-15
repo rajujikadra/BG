563aa37a06b86fd9bbfb3f4ef4410311db70edcb
Ashish <akchodavadiya@gmail.com>
2020-03-15 11:57:11 +0530

    page permission access restriction user wise

--- BG/Global.asax.cs
+++ BG/Global.asax.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using BG_Application.Data;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Security.Claims;
@@ -17,8 +18,38 @@ namespace BG
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
-            
+
             AntiForgeryConfig.UniqueClaimTypeIdentifier = ClaimTypes.Name;
+        }
+        protected void Application_PostAuthenticateRequest(Object sender, EventArgs e)
+        {
+            if (HttpContext.Current.Request.RawUrl.Contains("/Error"))
+                return;
+            if (User.Identity.IsAuthenticated)
+            {
+                string URL = HttpContext.Current.Request.RawUrl;
+                if (URL.Contains("admin"))
+                {
+                    var DB = new BG_DBEntities();
+                    string ID = DB.AspNetUsers.FirstOrDefault(x => x.Email.Trim() == User.Identity.Name.Trim()).Id;
+                    if (!string.IsNullOrEmpty(ID))
+                    {
+                        var MainMenuURL = DB.UserMenuPermissionMsts.Where(x => x.UserId == ID && x.MainMenuMst.Active == true).Select(y => y.MainMenuMst.URL.Trim().ToLower()).Distinct().ToList();
+                        var MenuURL = DB.UserMenuPermissionMsts.Where(x => x.UserId == ID && x.MenuMst.Active == true).Select(y => y.MenuMst.URL.Trim().ToLower()).Distinct().ToList();
+                        MainMenuURL.AddRange(MenuURL);
+                        MainMenuURL.Add("/");
+                        MainMenuURL.Add("/Account/LogOff".ToLower());
+                        MainMenuURL.Add("/admin/customers/new");
+                        MainMenuURL.Add("/admin/customers/register");
+                        MainMenuURL.Add("/admin/sales-person");
+                        bool IsMenuAccess = MainMenuURL.Contains(URL.ToLower().Trim());
+                        if (!IsMenuAccess)
+                        {
+                            HttpContext.Current.Response.Redirect("~/Error");
+                        }
+                    }
+                }
+            }
         }
     }
 }
