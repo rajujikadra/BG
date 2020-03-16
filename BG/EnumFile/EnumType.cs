32af15dc7e622f8b0b585c97579cd22a37e23f3b
Ashish <akchodavadiya@gmail.com>
2020-03-16 20:07:59 +0530

    column name

--- BG/EnumFile/EnumType.cs
+++ BG/EnumFile/EnumType.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Web;
 
@@ -7,11 +8,100 @@ namespace BG.EnumFile
 {
     public class EnumType
     {
-      public enum Token
+        public enum Token
         {
             grant_type,
             username,
             password
+        }
+
+        public enum StockColumnName
+        {
+            [Description("Stone ID")]
+            StoneID,
+            [Description("Cts")]
+            Cts,
+            [Description("Location")]
+            Location,
+            [Description("Report No")]
+            ReportNo,
+            [Description("Certificate")]
+            CertificateName,
+            [Description("Shape")]
+            ShapeName,
+            [Description("Size")]
+            Size,
+            [Description("Color")]
+            ColorName,
+            [Description("Purity")]
+            Purity,
+            [Description("Cut")]
+            Cut,
+            [Description("Polish")]
+            Polish,
+            [Description("Symmetry")]
+            Symmetry,
+            [Description("Flou.")]
+            Flou,
+            [Description("Rap")]
+            Rap,
+            [Description("Back")]
+            Disc,
+            [Description("Asking")]
+            Asking,
+            [Description("Amount")]
+            Amount,
+            [Description("S %")]
+            SPer,
+            [Description("S Rate")]
+            SRate,
+            [Description("S Amount")]
+            SAmount,
+            [Description("Length")]
+            Length,
+            [Description("Width")]
+            Width,
+            [Description("Depth")]
+            Depth,
+            [Description("Depth %")]
+            DepthPer,
+            [Description("Tab %")]
+            TablePer,
+            [Description("Crown Angle")]
+            CrownAngle,
+            [Description("Crown Height")]
+            CrownHeight,
+            [Description("Pav Angle")]
+            PavAngle,
+            [Description("Pav Height")]
+            PavHeight,
+            [Description("KeytoSymbol")]
+            KeyToSymbol,
+            [Description("Eye clean")]
+            EyeClean,
+            [Description("Girdle")]
+            Girdle,
+            [Description("Culet")]
+            Culet,
+            [Description("Star")]
+            Star,
+            [Description("Lower")]
+            Lower,
+            [Description("Milky")]
+            Milky,
+            [Description("Hearts & Arrow")]
+            HA,
+            [Description("Inscription")]
+            Inscription,
+            [Description("Comments")]
+            Comments,
+            [Description("VideoLink")]
+            VideoLink,
+            Sale,
+            Broker,
+            Hold,
+            Basket,
+            Inquiry
         }
     }
 }
--- BG/Global.asax.cs
+++ BG/Global.asax.cs
@@ -21,35 +21,35 @@ namespace BG
 
             AntiForgeryConfig.UniqueClaimTypeIdentifier = ClaimTypes.Name;
         }
-        protected void Application_PostAuthenticateRequest(Object sender, EventArgs e)
-        {
-            if (HttpContext.Current.Request.RawUrl.Contains("/Error"))
-                return;
-            if (User.Identity.IsAuthenticated)
-            {
-                string URL = HttpContext.Current.Request.RawUrl;
-                if (URL.Contains("admin"))
-                {
-                    var DB = new BG_DBEntities();
-                    string ID = DB.AspNetUsers.FirstOrDefault(x => x.Email.Trim() == User.Identity.Name.Trim()).Id;
-                    if (!string.IsNullOrEmpty(ID))
-                    {
-                        var MainMenuURL = DB.UserMenuPermissionMsts.Where(x => x.UserId == ID && x.MainMenuMst.Active == true).Select(y => y.MainMenuMst.URL.Trim().ToLower()).Distinct().ToList();
-                        var MenuURL = DB.UserMenuPermissionMsts.Where(x => x.UserId == ID && x.MenuMst.Active == true).Select(y => y.MenuMst.URL.Trim().ToLower()).Distinct().ToList();
-                        MainMenuURL.AddRange(MenuURL);
-                        MainMenuURL.Add("/");
-                        MainMenuURL.Add("/Account/LogOff".ToLower());
-                        MainMenuURL.Add("/admin/customers/new");
-                        MainMenuURL.Add("/admin/customers/register");
-                        MainMenuURL.Add("/admin/sales-person");
-                        bool IsMenuAccess = MainMenuURL.Contains(URL.ToLower().Trim());
-                        if (!IsMenuAccess)
-                        {
-                            HttpContext.Current.Response.Redirect("~/Error");
-                        }
-                    }
-                }
-            }
-        }
+        //protected void Application_PostAuthenticateRequest(Object sender, EventArgs e)
+        //{
+        //    if (HttpContext.Current.Request.RawUrl.Contains("/Error"))
+        //        return;
+        //    if (User.Identity.IsAuthenticated)
+        //    {
+        //        string URL = HttpContext.Current.Request.RawUrl;
+        //        if (URL.Contains("admin"))
+        //        {
+        //            var DB = new BG_DBEntities();
+        //            string ID = DB.AspNetUsers.FirstOrDefault(x => x.Email.Trim() == User.Identity.Name.Trim()).Id;
+        //            if (!string.IsNullOrEmpty(ID))
+        //            {
+        //                var MainMenuURL = DB.UserMenuPermissionMsts.Where(x => x.UserId == ID && x.MainMenuMst.Active == true).Select(y => y.MainMenuMst.URL.Trim().ToLower()).Distinct().ToList();
+        //                var MenuURL = DB.UserMenuPermissionMsts.Where(x => x.UserId == ID && x.MenuMst.Active == true).Select(y => y.MenuMst.URL.Trim().ToLower()).Distinct().ToList();
+        //                MainMenuURL.AddRange(MenuURL);
+        //                MainMenuURL.Add("/");
+        //                MainMenuURL.Add("/Account/LogOff".ToLower());
+        //                MainMenuURL.Add("/admin/customers/new");
+        //                MainMenuURL.Add("/admin/customers/register");
+        //                MainMenuURL.Add("/admin/sales-person");
+        //                bool IsMenuAccess = MainMenuURL.Contains(URL.ToLower().Trim());
+        //                if (!IsMenuAccess)
+        //                {
+        //                    HttpContext.Current.Response.Redirect("~/Error");
+        //                }
+        //            }
+        //        }
+        //    }
+        //}
     }
 }
