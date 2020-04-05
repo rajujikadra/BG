324680621950925d8d00e48a9687c442527a9861
Rajesh Jikadra <rajujikadra@gmail.com>
2020-04-05 11:55:16 +0530

    Add home page view model and get some datas

--- BG/Controllers/HomeController.cs
+++ BG/Controllers/HomeController.cs
@@ -17,6 +17,38 @@ namespace BG.Controllers
             {
                 return RedirectToAction("Login", "Account");
             }
+            var DB = new BG_DBEntities();
+            var model = new HomeViewModel();
+            model.Shape = DB.ShapeMsts.Where(x => x.Active == true).Select(c => new Shape()
+            {
+                ShapeCode = c.ShapeCode,
+                ShapeName = c.ShapeName,
+                ShapeAliasName = c.ShapeAliasName
+            }).ToList();
+            model.Color = DB.ColorMsts.Where(x => x.Active == true).Select(c => new Color()
+            {
+                ColorCode = c.ColorCode,
+                ColorName = c.ColorName,
+                ColorAliasName = c.ColorAliasName
+            }).ToList();
+            model.Cut = DB.CutMsts.Where(x => x.Active == true).Select(c => new Cut()
+            {
+                CutCode = c.CutCode,
+                CutAliasName = c.CutAliasName,
+                CutName = c.CutName
+            }).ToList();
+            model.Polish = DB.PolishMsts.Where(x => x.Active == true).Select(c => new Polish()
+            {
+                PolishCode = c.PolishCode,
+                PolishAliasName = c.PolishAliasName,
+                PolishName = c.PolishName
+            }).ToList();
+            model.Symmetry = DB.SymmetryMsts.Where(x => x.Active == true).Select(c => new Symmetry()
+            {
+                SymmetryCode = c.SymmetryCode,
+                SymmetryAliasName = c.SymmetryAliasName,
+                SymmetryName = c.SymmetryName
+            }).ToList();
             return View();
         }
 
---
+++ BG_Application/CustomDTO/HomeViewModel.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BG_Application.CustomDTO
+{
+    public class HomeViewModel
+    {
+        public List<Shape> Shape { get; set; }
+        public List<Color> Color { get; set; }
+        public List<Cut> Cut { get; set; }
+        public List<Polish> Polish { get; set; }
+        public List<Symmetry> Symmetry { get; set; }
+    }
+
+    public class Shape
+    {
+        public int ShapeCode { get; set; }
+        public string ShapeName { get; set; }
+        public string ShapeAliasName { get; set; }
+    }
+    public class Color
+    {
+        public int ColorCode { get; set; }
+        public string ColorName { get; set; }
+        public string ColorAliasName { get; set; }
+    }
+    public class Cut
+    {
+        public int CutCode { get; set; }
+        public string CutName { get; set; }
+        public string CutAliasName { get; set; }
+    }
+    public class Polish
+    {
+        public int PolishCode { get; set; }
+        public string PolishName { get; set; }
+        public string PolishAliasName { get; set; }
+    }
+    public class Symmetry
+    {
+        public int SymmetryCode { get; set; }
+        public string SymmetryName { get; set; }
+        public string SymmetryAliasName { get; set; }
+    }
+}
