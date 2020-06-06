d0bfdb050b9d990108b02affca956b290d158b75
Rajesh Jikadra <rajujikadra@gmail.com>
2020-06-06 21:29:43 +0530

    Changes on DNA report

---
+++ BG/Controllers/DNAController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BG.Controllers
+{
+    [Authorize]
+    public class DNAController : Controller
+    {
+        // GET: DNA
+        [Route("dna-report/{StoneID}")]
+        public ActionResult Index(string StoneID)
+        {
+            return View();
+        }
+    }
+}
