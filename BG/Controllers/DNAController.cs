61e4f4e0aac55e8333abcfa74f5c78ca400fd2f4
Rajesh Jikadra <rajujikadra@gmail.com>
2020-06-09 22:54:49 +0530

    Display diamond

--- BG/Controllers/DNAController.cs
+++ BG/Controllers/DNAController.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using BG.Models;
+using BG_Application.Data;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -13,7 +16,73 @@ namespace BG.Controllers
         [Route("dna-report/{StoneID}")]
         public ActionResult Index(string StoneID)
         {
-            return View();
+            var DB = new BG_DBEntities();
+            var Stone = DB.DiamondStocks.Where(x => x.StoneID.Trim().ToLower().Equals(StoneID.Trim().ToLower())).Select(y => new DNADisplayViewModel()
+            {
+                StoneID = y.StoneID,
+                CaratWeight = y.Cts.ToString(),
+                Clarity = DB.PurityMsts.FirstOrDefault(b => b.PurityCode == y.PurityCode).PurityName,
+                PurityCode = y.PurityCode,
+                Color = DB.ColorMsts.FirstOrDefault(b => b.ColorCode == y.ColorCode).ColorName,
+                ColorCode = y.ColorCode,
+                Cut = DB.CutMsts.FirstOrDefault(b => b.CutCode == y.CutCode).CutName,
+                CutCode = y.CutCode,
+                Shape = DB.ShapeMsts.FirstOrDefault(b => b.ShapeCode == y.ShapeCode).ShapeName,
+                ShapeCode = y.ShapeCode
+            }).FirstOrDefault();
+
+            if (Stone != null)
+            {
+                Stone.RelatedStone = DB.DiamondStocks.Where(x => x.PurityCode == Stone.PurityCode && x.ColorCode == Stone.ColorCode && x.CutCode == Stone.CutCode && x.ShapeCode == Stone.ShapeCode).Select(y => new RelatedStone()
+                {
+                    StoneID = y.StoneID,
+                    CaratWeight = y.Cts.ToString(),
+                    Clarity = DB.PurityMsts.FirstOrDefault(b => b.PurityCode == y.PurityCode).PurityName,
+                    PurityCode = y.PurityCode,
+                    Color = DB.ColorMsts.FirstOrDefault(b => b.ColorCode == y.ColorCode).ColorName,
+                    ColorCode = y.ColorCode,
+                    Cut = DB.CutMsts.FirstOrDefault(b => b.CutCode == y.CutCode).CutName,
+                    CutCode = y.CutCode,
+                    Shape = DB.ShapeMsts.FirstOrDefault(b => b.ShapeCode == y.ShapeCode).ShapeName,
+                    ShapeCode = y.ShapeCode
+                }).ToList();
+            }
+            FileInfo pdfFiles = GetFiles(Server.MapPath(@"~/DNAReports/" + StoneID), "pdf").ToList().FirstOrDefault();
+            if (pdfFiles != null)
+                Stone.PDFFileName = pdfFiles.Name;
+            List<FileInfo> JPG_images = GetFiles(Server.MapPath(@"~/DNAReports/" + StoneID), "jpg").ToList();
+            List<FileInfo> PNG_images = GetFiles(Server.MapPath(@"~/DNAReports/" + StoneID), "png").ToList();
+            List<FileInfo> JPEG_images = GetFiles(Server.MapPath(@"~/DNAReports/" + StoneID), "jpeg").ToList();
+            Stone.ImageNames = JPG_images.Select(c => c.Name).ToList();
+            Stone.ImageNames.AddRange(PNG_images.Select(c => c.Name).ToList());
+            Stone.ImageNames.AddRange(JPEG_images.Select(c => c.Name).ToList());
+            List<FileInfo> JSON_FIle = GetFiles(Server.MapPath(@"~/DNAReports/" + StoneID), "json").ToList();
+            foreach(var file in JSON_FIle)
+            {
+                using (StreamReader reader = file.OpenText())
+                {
+                    Stone.JSONString.Add(reader.ReadToEnd());
+                }                
+            }
+            return View(Stone);
+        }
+        public static IEnumerable<FileInfo> GetFiles(string path, string extension)
+        {
+            DirectoryInfo di = new DirectoryInfo(path);
+            if (di.Exists)
+            {
+                string search = string.Format("*.{0}", extension);
+                FileInfo[] files = di.GetFiles(search);
+                foreach (FileInfo fi in files)
+                {
+                    yield return fi;
+                }
+            }
         }
     }
 }
+
+
+
+
+
---
+++ BG/Models/DNADisplayViewModel.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BG.Models
+{
+    public class DNADisplayViewModel
+    {
+        public string StoneID { get; set; }
+        public string CaratWeight { get; set; }
+        public string Color { get; set; }
+        public int? ColorCode { get; set; }
+        public string Shape { get; set; }
+        public int? ShapeCode { get; set; }
+        public string Clarity { get; set; }
+        public int? PurityCode { get; set; }
+        public string Cut { get; set; }
+        public int? CutCode { get; set; }
+        public List<string> JSONString { get; set; }
+        public List<string> ImageNames { get; set; }
+        public string PDFFileName { get; set; }
+        public List<RelatedStone> RelatedStone { get; set; }
+    }
+
+    public class RelatedStone
+    {
+        public string StoneID { get; set; }
+        public string CaratWeight { get; set; }
+        public string Color { get; set; }
+        public int? ColorCode { get; set; }
+        public string Shape { get; set; }
+        public int? ShapeCode { get; set; }
+        public string Clarity { get; set; }
+        public int? PurityCode { get; set; }
+        public string Cut { get; set; }
+        public int? CutCode { get; set; }
+    }
+}
