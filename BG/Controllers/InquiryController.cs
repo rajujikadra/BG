3ea2be5ca3a999fe2f3a6503c832031e7fbbdae0
Rajesh <rbj@narola.email>
2020-06-30 11:10:34 +0530

    Added Inquiry page and some chagnes on some pages

---
+++ BG/Controllers/InquiryController.cs
@@ -0,0 +1,22 @@
+﻿using BG_Application.CustomDTO;
+using BG_Application.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using BG.Models;
+using Microsoft.Ajax.Utilities;
+
+namespace BG.Controllers
+{
+    [Authorize]
+    public class InquiryController : Controller
+    {
+        // GET: Inquiry
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
